0fe9ffff4953159fe07284a2c590ea64ca7b8022
MARCELO_DUZZI <marcelo.santonio@aluno.senai.br>
2025-08-14 10:21:04 -0300

    meus projetos

---
+++ resto_da_divisao.php
@@ -0,0 +1,9 @@
+<?php
+    $numero1 = 10;
+    $numero2 = 3;
+    $resto = $numero1 % $numero2;
+    echo "A divisão de 10 por 3 é: ". $numero1 / $numero2 ."<br>";
+    echo "O resto é: " . $resto;
+
+
+?>
