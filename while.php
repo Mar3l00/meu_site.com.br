0c246ada58aac7156236b8bc2841468a13bdeacc
MARCELO_DUZZI <marcelo.santonio@aluno.senai.br>
2025-08-21 15:55:47 -0300

    aaaaaaaaaa

---
+++ while.php
@@ -0,0 +1,18 @@
+<h3> Este é um While </h3>
+
+
+<?php
+$contador = 10;
+while ($contador <= 5) {
+    echo "numero: $contador<br>";
+    $contador++;
+}
+?>
+<H3>Este é um Do While</H3>
+<?php
+$contador = 10;
+do{
+    echo "Número: $contador<br>";
+    $contador++;
+}While ($contador <= 5);
+?>
